26bbb39e387be4592b9adbaaa990e39c6b5ac1cd
kumarprince411 <princekm5@live.com>
2023-12-24 20:36:01 +0530

    Image cards hamburger menu

---
+++ home-style.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".menu-mob-icon").click(function () {
+      $(".rt, .black").show();
+    });
+    $(".black, .x-close").click(function () {
+      $(".rt").hide();
+      $(this).hide();
+    });
+  });
+  
